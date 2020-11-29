8280ebf58c2350f3d9c846da70a8f98bd9c542b6
larenelg <lotusgenome@gmail.com>
2020-11-30 09:33:11 +1000

    remove this.destroy from default onError

--- pinoSeqStream.js
+++ pinoSeqStream.js
@@ -17,7 +17,7 @@ class PinoSeqStream extends stream.Writable {
     super();
 
     let { additionalProperties, logOtherAs, ...loggerConfig } = config == null ? {} : { ...config };
-    loggerConfig.onError = loggerConfig.onError || this.destroy;
+    loggerConfig.onError = loggerConfig.onError || function (e) { console.error('[PinoSeqStream] Log batch failed\n', e) };
     this._additionalProperties = additionalProperties;
     this._logOtherAs = logOtherAs;
     this._bufferTime = false;
