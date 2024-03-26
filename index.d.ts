8edece5060eb00920a3778cec0a471d46c8ddddb
Richard J <106497642+richard-stafflink@users.noreply.github.com>
2024-03-26 13:50:40 +1000

    index.d.ts: Expose the flush method on the return type

--- index.d.ts
+++ index.d.ts
@@ -11,7 +11,9 @@ declare namespace PinoSeq {
     onError?: (e: Error) => void;
   }
 
-  function createStream(config: PinoSeq.SeqConfig): Writable;
+  function createStream(config: PinoSeq.SeqConfig): Writable & { _logger: seq.Logger; flush: () => void };
+  // Or perhaps just:
+  // function createStream(config: PinoSeq.SeqConfig): Writable & { flush: () => void };
 }
 
 export = PinoSeq;
